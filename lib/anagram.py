215829b45669662ddb34f2660d32cdcffa6ea0c1
irene <irene.ndindaa@gmail.com>
2024-01-24 00:43:02 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, match):
+        self._match = []
+        for word in match:
+            if sorted(word) == sorted(self.word) and word != self.word:
+                self._match.append(word)
+        return self._match
+    
+    
+
+    
